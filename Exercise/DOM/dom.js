bc6910f768b0af948db61d165410fb9f17bf3cc9
DUONG NAM <duongquocnam224400@gmail.com>
2021-11-26 21:55:18 +0700

    Completed-Exercise-DOM-Js

---
+++ Exercise/DOM/dom.js
@@ -0,0 +1,25 @@
+// 1. Bấm vào nút, phông chữ, cỡ chữ và màu của văn bản đoạn văn sẽ được thay đổi.
+
+// 2. Viết một hàm JavaScript để nhận các giá trị Họ và Tên của biểu mẫu.
+
+// 3. Viết chương trình JavaScript để đặt màu nền của đoạn văn
+
+// 4. Đây là một tệp html mẫu có nút gửi. Viết một hàm JavaScript để nhận giá trị của các thuộc tính href, hreflang, rel, target và type của liên kết được chỉ định.
+
+// 5. Viết hàm JavaScript để thêm hàng vào bảng.
+
+// 6. Viết một hàm JavaScript chấp nhận hàng, cột, (để xác định một ô cụ thể) và một chuỗi để cập nhật nội dung của ô đó.
+
+// 7. Viết hàm JavaScript để tạo bảng, chấp nhận số hàng, số cột từ người dùng và nhập số hàng-cột làm nội dung (ví dụ: Hàng-0 Cột-0) của một ô.
+
+// 8. Viết chương trình JavaScript để xóa các mục khỏi danh sách thả xuống.
+
+// 9. Viết chương trình JavaScript để đếm và hiển thị các mục của danh sách thả xuống, trong cửa sổ cảnh báo.
+
+// 10. Viết chương trình JavaScript để tính thể tích khối cầu.
+
+// 11. Viết chương trình JavaScript để hiển thị một hình ảnh ngẫu nhiên (nhấp vào nút) từ danh sách sau.
+
+// 12. Viết chương trình JavaScript để đánh dấu các từ in đậm của đoạn văn sau khi di chuột qua một liên kết nhất định
+
+// 13. Viết chương trình JavaScript để lấy chiều rộng và chiều cao của cửa sổ (bất kỳ lúc nào cửa sổ được thay đổi kích thước).
